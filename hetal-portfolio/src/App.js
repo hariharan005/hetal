4fdc9f9151f8dee6c279d45669ecf5133255ce28
Hari Haran.c <harichella005@gmail.com>
2024-02-13 19:46:16 +0530

    changed the loader time and after 2 second home page will display

--- hetal-portfolio/src/App.js
+++ hetal-portfolio/src/App.js
@@ -1,12 +1,39 @@
 import './App.css';
-//import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Loader from './components/loader';
+import HomePage from './pages/home';
+//import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [showHello, setShowHello] = useState(false);
+
+  //simulate loading with a timeout
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  useEffect(() => {
+    if (!loading) {
+      const helloTimeout = setTimeout(() => {
+        setShowHello(true);
+      }, 1000);
+      return () => clearTimeout(helloTimeout);
+    }
+  }, [loading]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <Loader name="Hetal K.Pandya"/>
+        {loading ? (
+          <Loader name="Hetal K.Pandya"/>
+        ) : (
+          showHello && <HomePage />
+        )}
       </header>
     </div>
   );
