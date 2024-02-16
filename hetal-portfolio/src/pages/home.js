3fa6257b07ac74aea3cf1a821293d68e1e2fb359
Hari Haran.c <harichella005@gmail.com>
2024-02-16 11:15:26 +0530

    homepage updated similar to ui design

--- hetal-portfolio/src/pages/home.js
+++ hetal-portfolio/src/pages/home.js
@@ -1,28 +1,34 @@
 import React from "react";
 import "../css/home.css";
 import Header from "../components/header";
-import Footer from "../components/footer";
-import { Container } from "react-bootstrap";
+//import Footer from "../components/footer";
 import profileImage from "../assets/logo.png";
 
 const HomePage = () => {
   return (
     <>
       <Header />
-      <Container className="profile-container">
+      <section className="profile-container">
         <div className="profile">
-          <h1>Hi I'm Hetal</h1>
-          <h3>Psycologist</h3>
-          <article>
-            Experienced psychologist specializing in counseling for students and
-            working professionals. Accomplished junior faculty member with a
-            passion for promoting mental well-being and personal development.
-          </article>
-          <img src={profileImage} alt="Profile" title="Hetal"></img>
+          <div className="bio">
+            <h1>
+              Hi I'm <span>Hetal</span>
+            </h1>
+            <h3>Psycologist</h3>
+            <article>
+              Experienced psychologist specializing in counseling for students
+              and working professionals. Accomplished junior faculty member with
+              a passion for promoting mental well-being and personal
+              development.
+            </article>
+          </div>
+          <div className="profile-img">
+            <img src={profileImage} alt="Profile" title="Hetal"></img>
+          </div>
           <button>Know More</button>
           <button>Download</button>
         </div>
-      </Container>
+      </section>
     </>
   );
 };
